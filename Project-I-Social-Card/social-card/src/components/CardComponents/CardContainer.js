578eace5cb3034f46ac800ea79d8e3baeeac5165
vladmog <vladmog@gmail.com>
2019-05-28 03:11:04 -0700

    Finished most of MVP, some questions left to be answered

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner'
+import CardContent from './CardContent'
+
+
+const CardContainer = props => {
+    return (
+        <div className = "cardContainer" href = "https://www.reactjs.org">
+            <CardBanner />
+            <CardContent />
+        </div>
+    )
+}
+
+// let cardContainer = document.querySelector(".cardContainer");
+// cardContainer.addEventListener('click', function(){
+//     console.log("HELLO");
+// })
+
+export default CardContainer;
